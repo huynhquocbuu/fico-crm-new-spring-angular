f6755efdf28023eb58f528d1778fc3c7eb584c4d
BuuHQ <BuuHQ@tpb.vn>
2023-11-10 18:06:30 +0700

    cm6 angular 1

--- angular-frontend/crm-frontend/src/app/app.module.ts
+++ angular-frontend/crm-frontend/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {authInterceptorProviders} from "./shared/helpers/auth.interceptor";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterOutlet,
+    RouterLink,
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
